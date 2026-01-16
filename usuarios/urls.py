d857556f7a77e3d8b24b3867eab0e121fc9b08eb
IassineItuiriua <isslamiassine@gmail.com>
2026-01-16 18:03:49 +0200

    correcao envio de email

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -14,44 +14,41 @@ urlpatterns = [
     # =========================
     # RECUPERAÇÃO DE SENHA
     # =========================
-    # =========================
-# RECUPERAÇÃO DE SENHA
-# =========================
 
-path(
-    "password_reset_form/",
-    auth_views.PasswordResetView.as_view(
-        template_name="usuarios/password_reset_form.html",
-        email_template_name="usuarios/password_reset_email.html",
-        subject_template_name="usuarios/password_reset_subject.txt",
-        success_url="/usuarios/password_reset_done/",
+    path(
+        "password-reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="usuarios/password_reset_form.html",
+            email_template_name="usuarios/password_reset_email.html",
+            subject_template_name="usuarios/password_reset_subject.txt",
+            success_url="/usuarios/password_reset_done/",
+        ),
+        name="password_reset",
     ),
-    name="password_reset_form",
-),
 
-path(
-    "password_reset_done/",
-    auth_views.PasswordResetDoneView.as_view(
-        template_name="usuarios/password_reset_done.html"
+    path(
+        "password_reset_done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="usuarios/password_reset_done.html"
+        ),
+        name="password_reset_done",
     ),
-    name="password_reset_done",
-),
 
-path(
-    "password_reset_confirm/<uidb64>/<token>/",
-    auth_views.PasswordResetConfirmView.as_view(
-        template_name="usuarios/password_reset_confirm.html",
-        success_url="/usuarios/reset/done/",
+    path(
+        "password_reset_confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="usuarios/password_reset_confirm.html",
+            success_url="/usuarios/reset/done/",
+        ),
+        name="password_reset_confirm",
     ),
-    name="password_reset_confirm",
-),
 
-path(
-    "reset/done/",
-    auth_views.PasswordResetCompleteView.as_view(
-        template_name="usuarios/password_reset_complete.html"
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="usuarios/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
     ),
-    name="password_reset_complete",
-),
 
 ]
