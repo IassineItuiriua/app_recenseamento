de2350c8b0fe18caca34bcbed7dfb5d452eccaf7
IassineItuiriua <isslamiassine@gmail.com>
2026-01-20 23:39:30 +0200

    varias alteracoes de senha

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 
-from usuarios.forms import EmailPasswordResetForm
 from . import views
 
 app_name = "usuarios"
@@ -13,27 +12,33 @@ urlpatterns = [
     path("completar_perfil/", views.completar_perfil, name="completar_perfil"),
     path("painel/", views.painel, name="painel"),
 
-    # 1. Tela para digitar o e-mail
-    path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='usuarios/password_reset.html',
-        email_template_name='usuarios/password_reset_email.html',
-        subject_template_name='usuarios/password_reset_subject.txt',
-        success_url='/usuarios/password_reset/done/'
-    ), name='password_reset'),
+    # 1. Solicitação - Adicionamos o success_url e o caminho da pasta registration
+    path('reset_password/', 
+         auth_views.PasswordResetView.as_view(
+             template_name="registration/password_reset_form.html",
+             success_url=reverse_lazy('usuarios:password_reset_done') # Importante!
+         ), 
+         name='password_reset'),
 
-    # 2. Tela de confirmação de envio
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='usuarios/password_reset_done.html'
-    ), name='password_reset_done'),
+    # 2. E-mail enviado
+    path('reset_password_sent/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name="registration/password_reset_done.html"
+         ), 
+         name='password_reset_done'),
 
-    # 3. Link enviado por e-mail (token)
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='usuarios/password_reset_confirm.html',
-        success_url='/usuarios/reset/done/'
-    ), name='password_reset_confirm'),
+    # 3. Confirmar nova senha - Adicionamos o success_url
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="registration/password_reset_confirm.html",
+             success_url=reverse_lazy('usuarios:password_reset_complete') # Importante!
+         ), 
+         name='password_reset_confirm'),
 
-    # 4. Tela de sucesso final
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='usuarios/password_reset_complete.html'
-    ), name='password_reset_complete'),
+    # 4. Sucesso final
+    path('reset_password_complete/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name="registration/password_reset_complete.html"
+         ), 
+         name='password_reset_complete'),
 ]
